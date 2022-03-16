ed31b30fd58717213db02083b7b2d0c74b0b6800
randreas <randreas7296@gmail.com>
2022-03-16 18:35:42 -0400

    checked in code for Tuple

--- src/LSMTree/const.h
+++ src/LSMTree/const.h
@@ -1,3 +1,6 @@
 /*
 File to store Constants 
 */
+
+#define TERMINATE      INT_MIN
+#define DELETE_PLACEHOLDER  INT_MAX
--- src/LSMTree/tuple.h
+++ src/LSMTree/tuple.h
@@ -4,15 +4,56 @@
 
 using namespace std;
 
-class tuple{
+class Value {
+public:
+	std::vector<int> items;
+	bool visible = true;
+
+	value(){}
+
+	Value(std::vector<int> items) {
+		this->items = items;
+	}
+
+	Value(const Value& v) {
+		visible = v.visible;
+		items.insert(items.end(), v.items.begin(), v.items.end())''
+	}
+
+	bool IsDelMarker() const {
+        if (items.size() > 0) return items[0] == TERMINATE;
+        return false;
+    }
+
+}
+
+class Tuple{
 public:
 	int key;
 	Value value;
 
 public:
-	tuple(int key, Value value);
+	tuple(int key, Value v);
 
-	inline void setValue(Value value) {
-		this->value = value;
+	inline void setValue(Value v) {
+		this->value = v;
 	}
+
+	inline Value getValue() const{
+		return value;
+	}
+
+	inline int getKey() const{
+		return key;
+	}
+
+	std::string toString() const;
+
+	inline bool isDeleteMarker() const {
+		return value.isDeleteMarker();
+	}
+
+
+
+
 }
