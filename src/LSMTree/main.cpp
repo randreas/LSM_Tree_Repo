7e063530cd1665265719a199ed7906431ed5b723
troyyxk <xingkunyin@hotmail.com>
2022-03-22 21:25:09 -0400

    add main

---
+++ src/LSMTree/main.cpp
@@ -0,0 +1,47 @@
+//
+// Created by xingk on 3/22/2022.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+void executeQueryFile(ffstream* fs){
+    string line;
+    while (getline(*fs, line)) {
+        coutn << line;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc !=  4) {
+        cout << "USAGE: ./main <t> <data file path> <workload file path>\n";
+        return 1;
+    }
+
+    // constant, hyper parameter
+    int t = stoi(argv[1]);  // t stands for the amplifier
+    char* dataFilePath = argv[2];
+    char* workloadFilePath = argv[3];
+
+    // read and execute data file
+    ifstream dataFile;
+    dataFile.open(dataFilePath);
+    executeQueryFile(&dataFile);
+    dataFile.close();
+
+    cout<<"Hello World";
+
+    // read and execute workload file
+    ffstream workloadFile;
+    workloadFile.open(workloadFilePath);
+    executeQueryFile(&workloadFile);
+    workloadFile.close();
+
+    return 0;
+}
