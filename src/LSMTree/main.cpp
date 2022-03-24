523197459c1e3835caf06cee71f3b79def0e8786
Xingkun Yin <yinxingk@csa1.bu.edu>
2022-03-23 20:54:32 -0400

    start testing insert

--- src/LSMTree/main.cpp
+++ src/LSMTree/main.cpp
@@ -46,7 +46,8 @@ void executeCommand(LSMTree* lsmTree, string command) {
         cout << "Insert " << "key: " << key << " values: ";
         printIntVector(values);
         cout << "\n";
-        // TODO execute
+        // execute
+        lsmTree->addTuple(Tuple(key, Value(values)));
     } else if (elements[0] == "Q") {
         if (elements.size() != 2) {
             cout << "Q with incorrect size\n";
