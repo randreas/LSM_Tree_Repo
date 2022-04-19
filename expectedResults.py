3088c1b5355214ebdf14de7bb1b200160f3916d4
randreas <randreas7296@gmail.com>
2022-04-19 15:46:53 -0400

    Update expectedResults.py

--- expectedResults.py
+++ expectedResults.py
@@ -10,28 +10,28 @@ def readDataFile(readFile, writeFile):
 		if(len(a) == 1):
 			continue;
 		if(a[0] == "I"):
-			result.add(a[1]);
+			result.add(int(a[1]));
 			wf.write("Insert " + a[1] + " || currSize = " + str(len(result)) + "\n");
 		elif(a[0] == "D"):
 			if(len(a) == 3) :
-				result.discard(a[1]);
+				result.discard(int(a[1]));
 				wf.write("Delete " + a[1] + " || currSize = " +str(len(result))+ "\n")
 			elif(len(a) == 4) :
-				low = a[2]
-				high = a[3]
+				low = int(a[2])
+				high = int(a[3])
 				dRange = range(low,high)
 				for d in dRange:
 					result.discard(d)
 					wf.write("Delete " + d + " || currSize = " +str(len(result))+ "\n")
 		elif(a[0] == "Q"):
 			#query
-			result.find(a[1]);
+			result.find(int(a[1]));
 			wf.write("Found  " + a[1] + "\n")
 		elif(a[0] == "S"):
 			#range scan
 			low = a[1]
 			high = a[2]
-			selectRange = range(low,high)
+			selectRange = range(int(low),int(high))
 			rangeScanResult = result.intersection(selectRange)
 			wf.write("Found rangeScan " + rangeScanResult + "\n")
 
