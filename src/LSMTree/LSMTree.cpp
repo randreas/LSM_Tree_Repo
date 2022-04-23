36edc85fcba97eb81fe67e9b1aab0336a3278c73
randreas <randreas7296@gmail.com>
2022-04-23 11:14:52 -0400

    Update LSMTree.cpp

--- src/LSMTree/LSMTree.cpp
+++ src/LSMTree/LSMTree.cpp
@@ -15,20 +15,20 @@ LSMTree::LSMTree(int _initial_run_size, int _num_run_per_level, bool _isTiering)
 void LSMTree::addTuple(LSMTuple::Tuple* tuple) {
     // check if buffer is full
     // full, move run to level 1, clear buffer
-    cout << "in LSMTree.addTuple\n";
+  //  cout << "in LSMTree.addTuple\n";
     buffer->addTuple(tuple);
-    cout << "Buffer after insert: \n";
-    buffer->printRun();
+  //  cout << "Buffer after insert: \n";
+  //  buffer->printRun();
     if (buffer->isFull()) {
         //FileMeta* bufferFile = buffer->createFileMetaFromRun(0, 0);  // level 0, index 0
         cout << "Buffer is full, need to flush\n";
         Run* push_run = new Run(buffer->MAX_TUPLE_NUM);
         push_run->merge(buffer);
         //push_run->addTuple(tuple);
-        cout << "run merged\n";
-        push_run->printRun();
+    //    cout << "run merged\n";
+    //    push_run->printRun();
         mergeNMove(0, push_run);
-        cout << "finished merge and move\n";
+   //     cout << "finished merge and move\n";
         //remove(const_cast<char*>(bufferFile->filePath.c_str()));
         buffer->shallowClear();
     }
