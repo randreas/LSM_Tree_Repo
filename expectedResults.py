73cecbbfcc46616ecd2a1a4c47ca9a0b4568209e
randreas <randreas7296@gmail.com>
2022-04-25 13:21:48 -0400

    Update expectedResults.py

--- expectedResults.py
+++ expectedResults.py
@@ -49,7 +49,7 @@ def readDataFile(readFile, writeFile):
 
 				wf.write("]\n");
 			else:
-				wf.write("Did not find rangeScan [" + low + ", " + high + "]\n");
+				wf.write("Did not find rangeScan  [" + low + ", " + high + "]\n");
 
 
 def main(args):
